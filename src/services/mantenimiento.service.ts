45af75361eefd5a9867fe790f9c97f8bc3f9a11e
Jesus <Ing.estevezaquino@gmail.com>
2020-03-27 14:39:30 -0400

    Se creo el modulo Mantenimiento

--- src/services/mantenimiento.service.ts
+++ src/services/mantenimiento.service.ts
@@ -8,7 +8,15 @@ export class MantenimientoService {
 
   constructor(private http:HttpClient) {}
 
-  obtenerCLientes(){
-    this.http.get("");
+  obtenerProductos(){
+    this.http.get("https://localhost:44306/api/mantenimiento/obtener_productos");
+  }
+
+  obtenerProveedores(){
+    this.http.get("https://localhost:44306/api/mantenimiento/obtener_proveedores");
+  }
+
+  obtenerClientes(){
+    this.http.get("https://localhost:44306/api/mantenimiento/obtener_clientes");
   }
 }
