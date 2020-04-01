22d8532434c344a8ccc16666e0384bc8bd048d82
Jesus <Ing.estevezaquino@gmail.com>
2020-04-01 14:32:48 -0400

    Se elimino el componente de consultas, con el fin de crear su propio modulo en lo adelante.

--- src/app/procesos/procesos.module.ts
+++ src/app/procesos/procesos.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from '../shared/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProcesosRoutingModule } from './procesos-routing.module';
 import { ProcesosComponent } from './procesos.component';
 import { EntradasComponent } from './components/entradas/entradas.component';
 import { StockComponent } from './components/stock/stock.component';
-import { ConsultasComponent } from './components/consultas/consultas.component';
 import { FacturacionComponent } from './components/facturacion/facturacion.component';
 
 
 @NgModule({
-  declarations: [ProcesosComponent, EntradasComponent, StockComponent, ConsultasComponent, FacturacionComponent],
+  declarations: [ProcesosComponent, EntradasComponent, StockComponent, FacturacionComponent],
   imports: [
     CommonModule,
-    ProcesosRoutingModule
+    ProcesosRoutingModule,
+    MaterialModule
   ]
 })
 export class ProcesosModule { }
